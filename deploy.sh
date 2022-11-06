87c0c8375435e4e2439f877a1e1caca57e499160
MRPratama <mrpratama45@gmail.com>
2022-11-06 20:02:55 +0700

    upload deploy script

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:MRPratama45/vue-lp.git main
+git push -f git@github.com:MRPratama/vue-landingpage_2.git main:gh-pages
 
 cd -
